42622db6d380fa90d5fcfb1213806a4678cc5183
Dmytro Kovalchuk <dimasmith@gmail.com>
2023-03-26 14:11:41 +0300

    refactor: add utility function to interpret source

---
+++ examples/run_source.rs
@@ -0,0 +1,7 @@
+use l9_vm::interpret;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+    interpret("19 + 28")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
 //! Virtual machine to support running l9 toy programming language
+use crate::compiler::Compiler;
+use crate::lexer::Lexer;
+use crate::parser::Parser;
+use crate::vm::Vm;
+use std::error::Error;
+
 pub mod ast;
 pub mod chunk;
 pub mod compiler;
@@ -10,3 +16,16 @@ pub mod parser;
 pub mod trace;
 pub mod value;
 pub mod vm;
+
+/// Shortcut function to interpret the source code.
+pub fn interpret(source: &str) -> Result<(), Box<dyn Error>> {
+    let lexer = Lexer::new(source);
+    let mut parser = Parser::new(lexer);
+    let ast = parser.parse()?;
+    let mut compiler = Compiler::default();
+    let chunk = compiler.compile(&ast);
+    let mut vm = Vm::default();
+    vm.interpret(chunk)?;
+
+    Ok(())
+}
