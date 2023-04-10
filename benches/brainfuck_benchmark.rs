836ae6d1ab68043a045143af50f66c7fe7b23265
Dmytro Kovalchuk <dimasmith@gmail.com>
2023-04-11 01:10:25 +0300

    perf: add benchmarks

---
+++ benches/brainfuck_benchmark.rs
@@ -0,0 +1,32 @@
+use std::cell::RefCell;
+use std::error::Error;
+use std::fs::File;
+use std::rc::Rc;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use l9_vm::compiler::Compiler;
+use l9_vm::lexer::Lexer;
+use l9_vm::parser::Parser;
+use l9_vm::vm::Vm;
+
+fn interpret(source: &str) -> Result<(), Box<dyn Error>> {
+    let lexer = Lexer::new(source);
+    let mut parser = Parser::new(lexer);
+    let ast = parser.parse_program()?;
+    let mut compiler = Compiler::default();
+    let script = compiler.compile_script(ast)?;
+    let mut vm = Vm::with_io(Rc::new(RefCell::new(vec![])));
+    vm.run_script(script)?;
+
+    Ok(())
+}
+
+fn brainfuck_benchmark(c: &mut Criterion) {
+    let source_file = File::open("benches/brainfuck.l9").unwrap();
+    let src = std::io::read_to_string(source_file).unwrap();
+    c.bench_function("brainfuck", |b| b.iter(|| interpret(black_box(&src))));
+}
+
+criterion_group!(benches, brainfuck_benchmark);
+criterion_main!(benches);
