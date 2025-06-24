ecb758d854b97b03fcf7c4e8d0fe77277061c2d9
Dmytro Kovalchuk <dimasmith@gmail.com>
2025-06-24 09:37:27 +0300

    chore(deps): update dependencies

--- benches/brainfuck_benchmark.rs
+++ benches/brainfuck_benchmark.rs
@@ -3,7 +3,8 @@ use std::error::Error;
 use std::fs::File;
 use std::rc::Rc;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 
 use brainterpreter::compiler::Compiler;
 use brainterpreter::lexer::Lexer;
